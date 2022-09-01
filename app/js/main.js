80c8618b58cf250f032be5422de01b49f672f2bf
Nikolay Nuzhnyh <niknuzhnyh@gmail.com>
2022-09-01 14:31:31 +0300

    1.09

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,57 @@
+//sections
 const startScreen = document.querySelector(".startScreen");
 const descriptionScreen = document.querySelector(".descriptionScreen");
-const nextBtns = document.getElementsByClassName("nextBtn");
-
 const questionItem = document.getElementById("0");
+
+//form elements
+const first_name = document.getElementById("first_name");
+const second_name = document.getElementById("second_name");
+const last_name = document.getElementById("last_name");
+const genderGroup = document.getElementsByName("genderGroup");
+const birthdate = document.getElementById("birthdate");
+const inputsCollection = document.getElementsByTagName("input");
+const formBtn = document.getElementById("formBtn");
+
+
+//btns
+const nextBtns = document.getElementsByClassName("nextBtn");
 const yesBtn = document.getElementById("yes");
 const noBtn = document.getElementById("no");
+
+//user object
+const userInfo = {};
+
+//validation
+for (let index = 0; index < genderGroup.length; index++) {
+   const element = genderGroup[index];
+   element.addEventListener("change" ,() => {
+      userInfo.gender = element.value;
+   })
+}
+
+for (let index = 0; index < inputsCollection.length; index++) {
+   const element = inputsCollection[index];
+   if (element.type == "text" || element.type == "date") {
+      element.addEventListener("change" ,()=> {
+         if (first_name.value && second_name.value && last_name.value && birthdate.value && userInfo.gender) {
+            formBtn.disabled = false;
+         }
+      })
+   }
+}
+
+formBtn.addEventListener("click", (evt) => {
+   evt.preventDefault();
+})
+
+
+
+
+
+
+
+
+
 
 
 // yes/no buttons
@@ -26,7 +73,6 @@ noBtn.addEventListener("click", () => {
    }, 350);
 });
 
-
 //next buttons
 
 for (let index = 0; index < nextBtns.length; index++) {
@@ -39,7 +85,6 @@ for (let index = 0; index < nextBtns.length; index++) {
       parentSec.nextElementSibling.classList.remove("hidd");
    });
 }
-
 
 
 
